82f1ed27baf4648294a25cc9fe53d8bb6efbe01a
zbelial <zjytj@qq.com>
2021-11-28 19:26:47 +0800

    initial commit

---
+++ which-func-ts.el
@@ -0,0 +1,35 @@
+;;; which-func-ts.el --- implement which-func using tree-sitter -*- lexical-binding: t -*-
+
+;; Author: Gene Zhao <zjyzhaojiyang@gmail.com>
+;; Maintainer: Gene Zhao <zjyzhaojiyang@gmail.com>
+;; Version: 0.1
+;; Package-Requires: (tree-sitter)
+;; Homepage: https://github.com/zbelial/which-func-ts
+;; Keywords: which-func Emacs tree-sitter
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(provide 'which-func-ts)
+
+;;; which-func-ts.el ends here
